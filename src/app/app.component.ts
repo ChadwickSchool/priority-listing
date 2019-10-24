f61ac0c8b3f7f8e4aed7a465745a68015182d5f8
Dawson Goldsmith <dgoldsmith@chadwickschool.org>
2019-10-24 12:17:30 -0700

    feat: Add pushdown functionality to tasks when there are empty spaces below

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,21 +20,45 @@ export class AppComponent {
     'Task 5'
   ];
 
-  done = [
-  ];
+  choiceOne = [];
 
-  choiceTwo = [
+  choiceTwo = [];
 
-  ];
+  choiceThree = [];
+
+  choiceFour = [];
+
+  choiceFive = [];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
+    } else if (event.container.data.length === 0 || event.container.data === this.todo) {
+      console.log(event.currentIndex);
       transferArrayItem(event.previousContainer.data,
                         event.container.data,
                         event.previousIndex,
                         event.currentIndex);
+    } else {
+        let currentID = event.container.id;
+
+        if (currentID === "cdk-drop-list-1" && this.choiceTwo.length === 0) {
+          transferArrayItem(this.todo, this.choiceOne, event.previousIndex, 0);
+          transferArrayItem(this.choiceOne, this.choiceTwo, 1, 0);
+        }
+
+        if (currentID === "cdk-drop-list-2" && this.choiceThree.length === 0) {
+          transferArrayItem(this.todo, this.choiceTwo, event.previousIndex, 0);
+          transferArrayItem(this.choiceTwo, this.choiceThree, 1, 0);
+        }
+        if (currentID === "cdk-drop-list-3" && this.choiceFour.length === 0) {
+          transferArrayItem(this.todo, this.choiceThree, event.previousIndex, 0);
+          transferArrayItem(this.choiceThree, this.choiceFour, 1, 0);
+        }
+        if (currentID === "cdk-drop-list-4" && this.choiceFive.length === 0) {
+          transferArrayItem(this.todo, this.choiceFour, event.previousIndex, 0);
+          transferArrayItem(this.choiceFour, this.choiceFive, 1, 0);
+        }
     }
   }
 }
