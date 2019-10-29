013f637610cc816db1709eaf1a3478bacdfeba9d
Dawson Goldsmith <dgoldsmith@chadwickschool.org>
2019-10-29 10:19:09 -0700

    fix: Elminate bug which crashes website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,62 @@
-import {Component} from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Component } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
 
 /**
  * @title Drag&Drop connected sorting
  */
 @Component({
-  selector: 'app-component',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
-
+  selector: "app-component",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"]
 })
-
 export class AppComponent {
-  todo = [
-    'Task 1',
-    'Task 2',
-    'Task 3',
-    'Task 4',
-    'Task 5'
-  ];
+  todo = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"];
 
   choices = [[], [], [], [], []];
 
   choiceIDs() {
-    return this.choices.map((choice, i) => 'choice' + i).concat(['todo'])
+    return this.choices.map((choice, i) => "choice" + i).concat(["todo"]);
   }
   hasExtras() {
     for (let i = 0; i < this.choices.length; i++) {
-      if (this.choices[i].length >= 2) {
+      if (this.choices[i].length > 1) {
         return true;
       }
     }
     return false;
-
   }
+
   drop(event: CdkDragDrop<string[]>) {
-    console.log(event)
+    console.log(event);
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      console.log(event.previousIndex, event.currentIndex);
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else if (event.currentIndex === 0) {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
 
       while (this.hasExtras()) {
-        for (let i = 0; i < this.choices.length - 1; i++) {
-          if (this.choices[i].length >= 2){
-            transferArrayItem(this.choices[i], this.choices[i+1], 1, 0);
+        for (let i = 0; i < this.choices.length; i++) {
+          if (this.choices[i].length > 1) {
+            if (i < this.choices.length - 1) {
+              transferArrayItem(this.choices[i], this.choices[i + 1], 1, 0);
+            } else {
+              transferArrayItem(this.choices[i], this.todo, 1, 0);
+            }
           }
         }
       }
-   }
-}
+    }
+  }
 }
