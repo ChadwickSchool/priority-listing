29a6d29e56a7ca02aa1e4d76bc833d491f119193
Dawson Goldsmith <dgoldsmith@chadwickschool.org>
2019-12-03 10:03:55 -0800

    feat: Teacher Can Select Amount of Choices

--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -6,12 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teacher.component.scss']
 })
 export class TeacherComponent implements OnInit {
-  options = [];
+  selectedOption: string;
+  selectedOptionsArray: Array<number>;
+
+  options = [
+    {name: '2', value: 2},
+    {name: '3', value: 3},
+    {name: '4', value: 4},
+    {name: '5', value: 5},
+    {name: '6', value: 6},
+    {name: '7', value: 7},
+    {name: '8', value: 8},
+    {name: '9', value: 9},
+    {name: '10', value: 10}
+  ];
 
   constructor() { }
 
   ngOnInit() {
-    this.options.length = 5;
+    // this.options.length = 10;
+  }
+
+  updateOptions() {
+    this.selectedOptionsArray = new Array(parseInt(this.selectedOption, 10));
+    console.log(this.selectedOption, this.selectedOptionsArray);
   }
 
   submitOptions() {
