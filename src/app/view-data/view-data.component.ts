9356b6197f1418d98115396ec509a2f32c2143f9
Dawson Goldsmith <dgoldsmith@chadwickschool.org>
2019-12-19 08:30:49 -0800

    feat: Add algorithm to sort for winner

--- src/app/view-data/view-data.component.ts
+++ src/app/view-data/view-data.component.ts
@@ -1,22 +1,33 @@
-import { Component, OnInit } from "@angular/core";
-import { RankVotesService } from "../services/rank-votes.service";
-import { GetAllChoicesService } from "../services/get-all-choices.service";
-import { Observable } from "rxjs";
-import { Options } from "../shared/models/options";
-import { GetOptionsService } from "../services/get-options.service";
-import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { Component, OnInit } from '@angular/core';
+import { RankVotesService } from '../services/rank-votes.service';
+import { GetAllChoicesService } from '../services/get-all-choices.service';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Options } from '../shared/models/options';
+import { GetOptionsService } from '../services/get-options.service';
+import {
+  AngularFirestoreCollection,
+  AngularFirestore
+} from '@angular/fire/firestore';
 import { Choices } from '../shared/models/choices';
 
 @Component({
-  selector: "app-view-data",
-  templateUrl: "./view-data.component.html",
-  styleUrls: ["./view-data.component.scss"]
+  selector: 'app-view-data',
+  templateUrl: './view-data.component.html',
+  styleUrls: ['./view-data.component.scss']
 })
 export class ViewDataComponent implements OnInit {
   winner: string;
+  dataLeft: Choices[]; // List of student choices. (List of list of strings)
+  candidatesLeft: Options[]; // List of option names. (List of strings)
   choicesRef: AngularFirestoreCollection<Choices>;
   choices: Observable<Choices[]>;
-  constructor(private getOptionsService: GetOptionsService, private afs: AngularFirestore) {
+  newArray;
+  constructor(
+    private getOptionsService: GetOptionsService,
+    private afs: AngularFirestore,
+    private getAllChoicesService: GetAllChoicesService
+  ) {
     this.choicesRef = afs.collection<Choices>('choices');
     this.choices = this.choicesRef.valueChanges();
     // this.rankVotesService = new RankVotesService(candidates, data);
@@ -27,18 +38,32 @@ export class ViewDataComponent implements OnInit {
     // this.winner = this.rankVotesService.solve();
   }
 
-  TeacherOptions(): Observable<Options[]> {
-    console.log(
-      "get teacher options function: " + this.getOptionsService.getOptions()
-    );
-    return this.getOptionsService.getOptions();
+  async TeacherOptions() {
+    this.candidatesLeft = await this.getOptionsService
+      .getOptions()
+      .pipe(take(1))
+      .toPromise();
+    console.log('this.candidatesLeft:' + JSON.stringify(this.candidatesLeft));
+    return this.candidatesLeft;
   }
 
-  StudentChoices(): Observable<Choices[]> {
-    this.choices.subscribe(choices => {
-      console.log(choices);
+  async StudentChoices() {
+    this.newArray = [];
+    // this.choices.subscribe(choices => {
+    //   console.log(choices);
+    // });
+    this.dataLeft = await this.getAllChoicesService
+      .getStudentResponses()
+      .pipe(take(1))
+      .toPromise();
+    console.log('this.dataLeft: ' + JSON.stringify(this.dataLeft));
+    this.dataLeft.forEach(element => {
+      console.log(element.ranking);
+      let i = 0;
+      this.newArray[0] = element.ranking;
+      i++;
     });
-    console.log(this.choices);
-    return this.choices;
+    console.log('newArray: ' + this.newArray);
+    return this.dataLeft;
   }
 }
