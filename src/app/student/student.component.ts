5e5fe14dd0c2bcc6516a9a31f68262fe46ec37a1
Dawson Goldsmith <dgoldsmith@chadwickschool.org>
2020-01-23 11:56:52 -0800

    feat: Hide choices when student hasn't selected their survey yet

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -19,8 +19,9 @@ import { take } from 'rxjs/operators';
   styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
+  showChoices: boolean;
 
-  todo = [];
+  todo = ['Loading...'];
 
   assignedChoices = [];
 
@@ -40,6 +41,7 @@ export class StudentComponent implements OnInit {
 
   ngOnInit(): void {
     this.showOptions();
+    this.showChoices = false;
     // this.getOptionsService.getOptionsByName(this.surveyName).subscribe(options => {
     //   this.todo = options[0].tasks;
 
@@ -55,6 +57,7 @@ export class StudentComponent implements OnInit {
   }
 
   showTasks(name: string) {
+    this.showChoices = true;
     this.surveyName = name;
     console.log(this.surveyName);
     this.getOptionsService.getOptionsByName(this.surveyName).subscribe(options => {
